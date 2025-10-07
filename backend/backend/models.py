5794a4179ba1d33065b56a0c06c51a632dd7ba28
Milan <drownedmilan@gmail.com>
2025-10-07 22:24:47 +0200

    Clean tracked .pyc and __pycache__ files

--- backend/backend/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-
-class User(models.Model):
-	first_name=models.CharField(max_length=128)
-	last_name=models.CharField(max_length=128)
-	email_address=models.EmailField(max_length=128)
-	phone_number=PhoneNumberField(region='FR')
